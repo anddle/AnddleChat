3eaa6ad885db52fb0fb32d550e563b64ac31278c
YangGuang <diebu@126.com>
2016-10-10 15:44:27 +0800

    【YangGuang 20161010】 1.更新.gitignore文件 2.更新xml文件名称 3.删除多余文件

--- app/src/main/java/com/anddle/anddlechat/DeviceListActivity.java
+++ app/src/main/java/com/anddle/anddlechat/DeviceListActivity.java
@@ -34,7 +34,7 @@ public class DeviceListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.device_list_activity);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
